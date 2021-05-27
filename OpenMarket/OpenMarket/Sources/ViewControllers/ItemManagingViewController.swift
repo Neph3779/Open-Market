8f1bf62bec071a1bd4e7ca5180e50a196317fc61
Neph <tony3322@naver.com>
2021-05-27 17:34:21 +0900

    [Feat] #14 outerScrollView 구현

--- OpenMarket/OpenMarket/Sources/ViewControllers/ItemManagingViewController.swift
+++ OpenMarket/OpenMarket/Sources/ViewControllers/ItemManagingViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 class ItemManagingViewController: UIViewController {
+    let outerScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .systemBackground
+        return scrollView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setOuterScrollView()
+    }
+
+    func setOuterScrollView() {
+        view.addSubview(outerScrollView)
+        NSLayoutConstraint.activate([
+            outerScrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            outerScrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            outerScrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            outerScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
