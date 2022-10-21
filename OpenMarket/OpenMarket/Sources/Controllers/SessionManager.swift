9ab5ac95f1271feff610d2bdb715b42d7ddcd41a
Neph <tony3322@naver.com>
2022-10-21 10:10:43 +0900

    [Fix] path 시작이 /로 안되어있던 것 수정

--- OpenMarket/OpenMarket/Sources/Controllers/SessionManager.swift
+++ OpenMarket/OpenMarket/Sources/Controllers/SessionManager.swift
@@ -26,22 +26,25 @@ enum HTTPMethod: String {
 }
 
 enum RequestURLPath {
-    static let host = "https://openmarket.yagom-academy.kr"
+    static var urlComponents: URLComponents = {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "openmarket.yagom-academy.kr"
+        return urlComponents
+    }()
 
     static func healthCheck() throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/healthChecker"
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
         return url
     }
 
     static func getProductList(pageNumber: Int, itemsPerPage: Int = 100) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/api/products"
         urlComponents.queryItems = [
-            .init(name: "page_no", value: String(pageNumber )),
+            .init(name: "page_no", value: String(pageNumber)),
             .init(name: "items_per_page", value: String(itemsPerPage))
         ]
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
@@ -49,36 +52,31 @@ enum RequestURLPath {
     }
 
     static func getProductDetail(productId: Int) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/api/products/\(productId)"
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
         return url
     }
     static func postProduct() throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/api/products"
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
         return url
     }
     static func checkDeleteURI(productId: Int) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/api/products/\(productId)/archived"
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
         return url
     }
     static func modifyProduct(productId: Int) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/api/products/\(productId)"
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
         return url
     }
     static func deleteProduct(deleteURI: String) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.host = host
+        var urlComponents = urlComponents
         urlComponents.path = "/api/products/\(deleteURI)"
         guard let url = urlComponents.url else { throw OpenMarketError.invalidURL }
         return url
